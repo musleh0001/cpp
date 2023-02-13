555e2728945f51e8e67ce14bab88bc2a9e63d17e
musleh0001 <khan.muslehuddin0001@gmail.com>
2023-02-13 09:40:35 +0600

    adds subarray algo

---
+++ imp_subarray_with_given_sum.cpp
@@ -0,0 +1,46 @@
+// N = 5, Sum = 12
+// A[] = {1, 2, 3, 7, 5}
+// Output: 2 4
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n, s;
+    cin >> n >> s;
+
+    int arr[n];
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int i=0, j=0, st=-1, en=-1, sum=0;
+    while (j < n && sum + arr[j] <= s) {
+        sum += arr[j];
+        j++;
+    }
+
+    if (sum == s) {
+        cout << i+1 << " " << j << endl;
+        return 0;
+    }
+
+    while (j < n) {
+        sum += arr[j];
+        while (sum > s) {
+            sum -= arr[i];
+            i++;
+        }
+
+        if (sum == s) {
+            st = i+1;
+            en = j+1;
+            break;
+        }
+        j++;
+    }
+
+    cout << st << " " << en << endl;
+
+    return 0;
+}
